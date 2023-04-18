746dda4259774ea292c8617c7678f41a83b5c764
ykztenoks <danielskonetzky7@gmail.com>
2023-04-18 00:29:58 -0300

    :bricks: check answer for each q-type

--- src/components/questionTypes/cliqueImagem.tsx
+++ src/components/questionTypes/cliqueImagem.tsx
@@ -1,18 +1,62 @@
+import api from "@/api/api";
+import AuthContext from "@/context/authContext";
+import { useContext, useEffect, useState } from "react";
 import ReactPlayer from "react-player";
 export default function CliqueImagem({ question }: { question: Question }) {
+  const [message, setMessage] = useState("");
+  const [userAnswer, setUserAnswer] = useState("");
+
+  const checkAnswer = async (answer: string) => {
+    if (!answer || question.answer.toString() !== answer.toString()) {
+      setMessage("Resposta incorreta :/");
+    }
+    if (question.answer.toString() === answer.toString()) {
+      try {
+        const addPoints = await api.get("/user/add-points");
+        console.log(addPoints);
+        setMessage("resposta certa! parabéns");
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
+  useEffect(() => {
+    console.log(userAnswer);
+  }, [userAnswer]);
   return (
-    <>
-      <div id="heading">
-        <ReactPlayer url={question.heading} />
+    <main className="flex justify-center items-center flex-col mt-8">
+      <div id="heading" className="mb-6 w-[80vw] h-[40vh]">
+        <ReactPlayer
+          url={question.heading}
+          playing={true}
+          loop={true}
+          muted={true}
+          height={"40vh"}
+          width={"80vw"}
+        />
       </div>
-      <div id="question">
+      <div id="question" className="mb-6">
         <h2>{question.questions}</h2>
       </div>
-      <div id="options">
-        {question.options.map((option, i) => (
-          <img key={i} src={`${option}`} alt={`option ${i}`} />
-        ))}
+      <div className="flex flex-row  h-[30vh] gap-10">
+        {question.options.map((option, i) => {
+          return (
+            <img
+              key={i}
+              src={option}
+              alt={`opção ${i}`}
+              className="w-[25vw] h-[20vh] object-contain"
+              onClick={(e) => {
+                setUserAnswer(`${i}`);
+                checkAnswer(userAnswer);
+              }}
+            />
+          );
+        })}
       </div>
-    </>
+
+      {message && <p>{message}</p>}
+    </main>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,50 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
-    './src/app/**/*.{js,ts,jsx,tsx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      keyframes: {
+        openmenu: {
+          "0%": { transform: "scaleX(0)" },
+          "10%": { transform: "scaleX(0.1)" },
+          "20%": { transform: "scaleX(0.2)" },
+          "30%": { transform: "scaleX(0.3)" },
+          "40%": { transform: "scaleX(0.4)" },
+          "50%": { transform: "scaleX(0.5)" },
+          "60%": { transform: "scaleX(0.6)" },
+          "70%": { transform: "scaleX(0.7)" },
+          "80%": { transform: "scaleX(0.8)" },
+          "90%": { transform: "scaleX(0.9)" },
+          "100%": { transform: "scaleX(1)" },
+        },
+        closemenu: {
+          "100%": { transform: "scaleX(1)" },
+          "90%": { transform: "scaleX(0.9)" },
+          "80%": { transform: "scaleX(0.8)" },
+          "70%": { transform: "scaleX(0.7)" },
+          "60%": { transform: "scaleX(0.6)" },
+          "50%": { transform: "scaleX(0.5)" },
+          "40%": { transform: "scaleX(0.4)" },
+          "30%": { transform: "scaleX(0.3)" },
+          "20%": { transform: "scaleX(0.2)" },
+          "10%": { transform: "scaleX(0.1)" },
+          "0%": { transform: "scaleX(0)" },
+        },
+      },
+      animation: {
+        openmenu: "openmenu 0.2s ease-in-out",
+        closemenu: "closemenu 0.2 ease-out",
       },
     },
   },
   plugins: [],
-}
+};

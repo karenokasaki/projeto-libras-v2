273ff44ad69f1fd741c3c7664ab5dad904907946
ykztenoks <danielskonetzky7@gmail.com>
2023-05-11 03:21:58 -0300

    :sparkles: question layout

--- src/components/questionTypes/escolhaFrase.tsx
+++ src/components/questionTypes/escolhaFrase.tsx
@@ -17,12 +17,13 @@ export default function EscolhaFrase({
     //checa se errou
     if (userAnswer !== +question.answer) {
       await api.get(`/user/remove-points/${id}`);
-      setMessage("resposta errada");
+      setMessage("Resposta errada 😖");
       return;
     }
 
     try {
       await api.get(`/user/add-points/${id}`);
+      setMessage("");
       setUserAnswer(undefined);
       setAnswered(true);
     } catch (error) {
@@ -46,13 +47,13 @@ export default function EscolhaFrase({
           loop={true}
           controls={true}
           muted={true}
-          height={"auto"}
-          width={"auto"}
+          height={"40vh"}
+          width={"100vw"}
         />
       </div>
       <div
         id="options"
-        className="flex flex-col gap-7 py-4 lg:flex-row lg:gap-16 lg:py-10"
+        className="flex flex-col gap-4 py-2 lg:flex-row lg:gap-16 lg:py-6"
       >
         {question.options.map((option, i) => (
           <div
@@ -60,14 +61,14 @@ export default function EscolhaFrase({
             onClick={() => {
               setUserAnswer(i);
             }}
-            className=" bg-[#4c96d3] hover:text-white shadow-md transition-all font-semibold rounded-xl p-6 text-slate-200 text-center text-3xl tracking-[0.2em] "
+            className=" bg-[#4c96d3] hover:text-white shadow-md transition-all font-semibold rounded-xl p-4 text-slate-200 text-center text-2xl tracking-[0.2em] "
           >
             {option}
           </div>
         ))}
       </div>
       {message && (
-        <p className="bg-[#FFD966] p-4 rounded-xl text-slate-700 font-semibold text-xl shadow-lg tracking-wide mt-4">
+        <p className="bg-[#FFD966] p-4 rounded-xl text-slate-700 font-semibold text-xl shadow-lg tracking-wide mb-4">
           {message}
         </p>
       )}
